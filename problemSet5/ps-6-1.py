21bb5f9110736f9ed553dcc6fc793516fc1657c6
naxrevlis <me@naxrevlis.com>
2015-10-06 13:59:33 +0300

    Problem Set 5 and 6

---
+++ problemSet5/ps-6-1.py
@@ -0,0 +1,15 @@
+def swapSort(L):
+    """ L is a list on integers """
+    print "Original L: ", L
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if L[j] < L[i]:
+                # the next line is a short
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j]
+                print L
+                print "Final L: ", L
+
+
+L = [3,4,5,3,87,43,23,56]
+print swapSort(L)
